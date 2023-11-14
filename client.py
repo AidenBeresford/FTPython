1d51f7f2abca2e0dea120749650312f4fd5f1788
Aiden Beresford <aidenberes@gmail.com>
2023-11-13 21:26:28 -0600

    Add a makefile data receiver

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 
-FTP_PORT = 21
+FTP_PORT = 21  # default FTP port
+LINE_CAPACITY = 4096  # maximum size of a line read in bytes
 
 
 class Client:
@@ -9,6 +10,7 @@ class Client:
     source_address = None
     encoding = None
     timeout = None
+    file = None
     my_socket = None
 
     def __init__(self, host='', port=FTP_PORT, source_address=None, encoding='utf-8', timeout=socket.getdefaulttimeout()):
@@ -23,6 +25,21 @@ class Client:
     def _connect(self):  # only called during initialization
         if self.host is not None:
             self.my_socket = socket.create_connection((self.host, self.port), self.timeout, self.source_address)
+            self.file = self.my_socket.makefile('r', encoding=self.encoding)
+
+    def next_line(self):
+        line = self.file.readline(LINE_CAPACITY)  # 4096 is our maximum line size in bytes
+
+        if len(line) > LINE_CAPACITY:
+            print("Bytes received exceed capacity of " + str(LINE_CAPACITY) + " bytes.")
+
+        # look for any line terminators and remove them if they exist
+        if line[-2:] == '\r\n':
+            line = line[:-2]
+        elif line[-1:] in '\r\n':
+            line = line[:-1]
+
+        return line
 
     def close(self):  # close our connection if it exists
         if self.my_socket is not None:
--- main.py
+++ main.py
@@ -10,4 +10,7 @@ source_address = (source_host, int(source_port))
 encoding = args.encode
 
 FTP = Client(host, port, source_address, encoding)
+
+print(FTP.next_line(), end="")  # end="" just removes the newline that python adds
+
 FTP.close()
